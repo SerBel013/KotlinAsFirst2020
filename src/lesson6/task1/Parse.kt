2f686df2bd73d812ea50a48a6d78668e1db2532d
SelBel013 <gogulina.aa@edu.spbstu.ru>
2020-11-09 18:28:27 +0300

    6

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -2,6 +2,8 @@
 
 package lesson6.task1
 
+import java.lang.NumberFormatException
+
 // Урок 6: разбор строк, исключения
 // Максимальное количество баллов = 13
 // Рекомендуемое количество баллов = 11
@@ -74,7 +76,10 @@ fun main() {
  * Обратите внимание: некорректная с точки зрения календаря дата (например, 30.02.2009) считается неверными
  * входными данными.
  */
-fun dateStrToDigit(str: String): String = TODO()
+fun dateStrToDigit(str: String): String = TODO() // {
+//   val parts = str.split(" ")
+
+//}
 
 /**
  * Средняя (4 балла)
@@ -149,7 +154,14 @@ fun plusMinus(expression: String): Int = TODO()
  * Вернуть индекс начала первого повторяющегося слова, или -1, если повторов нет.
  * Пример: "Он пошёл в в школу" => результат 9 (индекс первого 'в')
  */
-fun firstDuplicateIndex(str: String): Int = TODO()
+fun firstDuplicateIndex(str: String): Int {
+    var k = 0
+    val parts = str.toLowerCase().split(" ")
+    if (parts.size < 2) return -1
+    for (i in 1 until parts.size)
+        if (parts[i] == parts[i - 1]) return k else k += parts[i - 1].length + 1
+    return -1
+}
 
 /**
  * Сложная (6 баллов)
@@ -162,7 +174,25 @@ fun firstDuplicateIndex(str: String): Int = TODO()
  * или пустую строку при нарушении формата строки.
  * Все цены должны быть больше нуля либо равны нулю.
  */
-fun mostExpensive(description: String): String = TODO()
+fun mostExpensive(description: String): String {
+    try {
+        val list = description.split("; ")
+        var max1 = -1.0
+        var name = ""
+        for (i in list.indices) {
+            val k = list[i].split(" ")
+            if (k[1].toDouble() > max1) {
+                max1 = k[1].toDouble()
+                name = k[0]
+            }
+        }
+        return name
+    } catch (e: IndexOutOfBoundsException) {
+        return ""
+    } catch (e: NumberFormatException) {
+        return ""
+    }
+}
 
 /**
  * Сложная (6 баллов)
